d39c482fa66ebd55eb23cdebe6a3b00458f0d3c8
damia <damiavidal@inspladelestany.cat>
2025-01-28 18:33:58 +0100

    Initial commit

---
+++ app/src/main/java/cat/dam/mindspeak/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package cat.dam.mindspeak.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+val BackgroundLight = Color(0xFFFFFFFF)
+val SecondaryColor = Color(0xFF0C99FF)
+val thirdColor = Color(0xFFD8EEFF)
+val DarkGray = Color(0xFF434343)
+val BackgroundDark = Color(0xFF272727)
+val BottomBarDark = Color(0xFF1B1B1B)
+val DarkThirdColor = Color(0xFF23394A)
