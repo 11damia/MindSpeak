4c1f9ea2bc9d36768ada7b8019f065af8b345070
damia <damiavidal@inspladelestany.cat>
2025-02-05 18:56:41 +0100

    Logo

---
+++ app/src/main/java/cat/dam/mindspeak/HomeUser.kt
@@ -0,0 +1,10 @@
+package cat.dam.mindspeak
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ProvidableCompositionLocal
+
+@Composable
+fun Inicio(LocalCustomColors: ProvidableCompositionLocal<CustomColors>) {
+
+}
--- app/src/main/java/cat/dam/mindspeak/MainActivity.kt
+++ app/src/main/java/cat/dam/mindspeak/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -15,6 +16,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Text
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
@@ -31,8 +33,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -133,7 +140,27 @@ fun MyApp() {
     }
 }
 
-
+@Composable
+fun TopBar(){
+    Row {
+        Text(
+            text = "MIND",
+            style = TextStyle(
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = DarkGray,
+            )
+        )
+        Text(
+            text = "SPEAK",
+            style = TextStyle(
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = LocalCustomColors.current.secondary,
+            )
+        )
+    }
+}
 
 @Composable
 fun BottomBar(
@@ -193,17 +220,14 @@ fun NavigationHost(
     navController: NavHostController,
 ) {
     NavHost(navController = navController, startDestination = "inicio") {
-        composable("inicio") { Inicio(navController) }
-        composable("Info") { Inicio(navController) }
+        composable("inicio") { Inicio(LocalCustomColors) }
+        composable("Info") { Info() }
 
     }
 }
-@Composable
-fun Inicio(navController: NavHostController){
 
-}
 @Composable
-fun Info(navController: NavHostController){
+fun Info(){
 
 }
 
@@ -211,5 +235,6 @@ fun Info(navController: NavHostController){
 @Composable
 fun GreetingPreview() {
     MindSpeakTheme {
+        TopBar()
     }
 }
