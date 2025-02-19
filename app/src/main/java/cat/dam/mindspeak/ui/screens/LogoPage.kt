3dd20604d640d27d3096cea77f969a55ded2c074
damia <damiavidal@inspladelestany.cat>
2025-02-19 19:51:47 +0100

    Logo page

--- app/src/main/java/cat/dam/mindspeak/ui/screens/LogoPage.kt
+++ app/src/main/java/cat/dam/mindspeak/ui/screens/LogoPage.kt
@@ -2,6 +2,7 @@ package cat.dam.mindspeak.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,7 +10,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -22,12 +25,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import androidx.navigation.NavHostController
 import cat.dam.mindspeak.R
 import cat.dam.mindspeak.ui.theme.DarkGray
@@ -35,58 +40,105 @@ import cat.dam.mindspeak.ui.theme.LocalCustomColors
 
 @Composable
 fun LogoPage(navController: NavHostController) {
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        /*
         Image(
-            painter = painterResource(id = R.drawable.logo),
+            painter = painterResource(id = R.drawable.circulo2),
             contentDescription = "image description",
-            contentScale = ContentScale.FillBounds
-        )
-        Row {
-            Text(
-                text = "MIND",
-                color = LocalCustomColors.current.text2,
-                fontWeight = FontWeight.ExtraBold,
-                fontSize = 32.sp,
-                modifier = Modifier.padding(bottom = 32.dp)
-            )
-            Text(
-                text = "SPEAK",
-                color = LocalCustomColors.current.secondary,
-                fontWeight = FontWeight.ExtraBold,
-                fontSize = 32.sp,
-                modifier = Modifier.padding(bottom = 32.dp)
-            )
-        }
-        Button(
-            onClick = { navController.navigate("login")},
-            shape = RoundedCornerShape(10.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = LocalCustomColors.current.secondary
-            ),            modifier = Modifier
-                .width(300.dp)
-                .height(50.dp)
-        ) {
-            Text("Iniciar Sesión")
-        }
-        Spacer(Modifier.height(8.dp))
-        Button(
-            onClick = { navController.navigate("signup")},
-            shape = RoundedCornerShape(10.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = DarkGray
-            ),            modifier = Modifier
-                .width(300.dp)
-                .height(50.dp),
-        ) {
-            Text("Registrarse")
-        }
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.TopStart)
+                .zIndex(-1f)
+                .graphicsLayer {
+                    rotationZ = 180f // Rota la imagen 180 grados
+                }
+        )*/
 
+        // Contenido centrado
+        LazyColumn(
+            modifier = Modifier
+                .align(Alignment.Center),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            item {
+                Image(
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = "image description",
+                    contentScale = ContentScale.FillBounds
+                )
+                Spacer(Modifier.height(8.dp))
+            }
+            item {
+                Row {
+                    Text(
+                        text = "MIND",
+                        color = LocalCustomColors.current.text2,
+                        fontWeight = FontWeight.ExtraBold,
+                        fontSize = 32.sp,
+                        modifier = Modifier.padding(bottom = 32.dp)
+                    )
+                    Text(
+                        text = "SPEAK",
+                        color = LocalCustomColors.current.secondary,
+                        fontWeight = FontWeight.ExtraBold,
+                        fontSize = 32.sp,
+                        modifier = Modifier.padding(bottom = 32.dp)
+                    )
+                }
+                Spacer(Modifier.height(8.dp))
+
+            }
+            item {
+                Button(
+                    onClick = { navController.navigate("login") },
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = LocalCustomColors.current.secondary
+                    ),
+                    modifier = Modifier
+                        .width(300.dp)
+                        .height(50.dp)
+                ) {
+                    Text("Iniciar Sesión")
+                }
+                Spacer(Modifier.height(8.dp))
+            }
+            item {
+                Button(
+                    onClick = { navController.navigate("signup") },
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = DarkGray
+                    ),
+                    modifier = Modifier
+                        .width(300.dp)
+                        .height(50.dp)
+                ) {
+                    Text("Registrarse")
+                }
+                Spacer(Modifier.height(20.dp))
+            }
+        }
+        /*
+        Image(
+            painter = painterResource(id = R.drawable.circulo2),
+            contentDescription = "image description",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomEnd)
+                .zIndex(-1f)
+                /*
+                .graphicsLayer {
+                    rotationZ = 180f // Rota la imagen 180 grados
+                }*/
+        )
+        */
 
     }
 }
