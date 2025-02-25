62b1d3b6638f26db6bc9f9adc6e148a854f639df
damia <damiavidal@inspladelestany.cat>
2025-02-25 19:07:41 +0100

    UploadResource.kt

---
+++ app/src/main/java/cat/dam/mindspeak/ui/screens/UploadResource.kt
@@ -0,0 +1,74 @@
+package cat.dam.mindspeak.ui.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import cat.dam.mindspeak.ui.theme.LocalCustomColors
+
+@Preview
+@Composable
+fun UploadResourceApp() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(text = "Subir un recurso", color = LocalCustomColors.current.text1,fontSize = 20.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(text = "Título del ejercicio:", color = LocalCustomColors.current.text1)
+        OutlinedTextField(
+            value = "Ejercicio n°1",
+            onValueChange = {},
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        UploadSection(title = "Subir una imagen", icon = Icons.Default.Person)
+        UploadSection(title = "Subir un video", icon = Icons.Default.Person)
+        UploadSection(title = "Grabar audio / Subir audio", icon = Icons.Default.Person)
+    }
+}
+
+@Composable
+fun UploadSection(title: String, icon: ImageVector) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(text = title, fontWeight = FontWeight.Medium, color = LocalCustomColors.current.text1)
+        Spacer(modifier = Modifier.height(8.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .background(LocalCustomColors.current.third, shape = RoundedCornerShape(8.dp)),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(imageVector = icon, contentDescription = title, modifier = Modifier.size(24.dp))
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
