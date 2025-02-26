668454be8ef526e53db814fae0d4cf55e6bdc73c
damia <damiavidal@inspladelestany.cat>
2025-02-26 18:15:26 +0100

    UploadResource.kt

--- app/src/main/java/cat/dam/mindspeak/ui/screens/UploadResource.kt
+++ app/src/main/java/cat/dam/mindspeak/ui/screens/UploadResource.kt
@@ -13,14 +13,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
-
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
